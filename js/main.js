5cd1fdd71306e98f2ad0d0fe3281f111b3ca7a93
sergeome <sergeome@gmail.com>
2016-11-12 19:12:24 -0600

    Implemented part of mobile and JS refactoring.

--- js/main.js
+++ js/main.js
@@ -1,39 +1,80 @@
-var tabContent;
-var tabLinks;
+lifeBlog = (function ($) {
+  var o = {};
 
-tabContent = document.getElementsByClassName("tab-panel");
-tabLinks = document.getElementsByClassName("tabs-navigation__item");
+  o.showTabs = function () {
+    var tabContent = $(".tab-panel");
+    var tabLinks = $(".tabs-navigation__item");
 
-function openTab(targetTab) {
+    function openTab(event) {
 
-  var indexOfTargetTab;
+      var indexOfTargetTab;
+      var clickedTab = event.currentTarget;
 
-  for (var z = 0; z < tabLinks.length; z++) {
-    if (tabLinks[z] === targetTab) {
-      indexOfTargetTab = z;
-      break;
+
+      for (var z = 0; z < tabLinks.length; z++) {
+        if (tabLinks[z] === clickedTab) {
+          indexOfTargetTab = z;
+          break;
+        }
+      }
+
+      for (var i = 0; i < tabContent.length; i++) {
+        tabContent[i].className = "tab-panel hidden";
+      }
+
+      for (var j = 0; j < tabLinks.length; j++) {
+        tabLinks[j].className = "tabs-navigation__item";
+      }
+
+      $(tabLinks[indexOfTargetTab]).toggleClass("tabs-navigation__item--active");
+      $(tabContent[indexOfTargetTab]).toggleClass("hidden");
     }
-  }
 
-  for (var i = 0; i < tabContent.length; i++) {
-    tabContent[i].className = "tab-panel hidden";
-  }
+    function addEventListeners() {
 
-  for (var j = 0; j < tabLinks.length; j++) {
-    tabLinks[j].className = "tabs-navigation__item";
-  }
+      for (var j = 0; j < tabLinks.length; j++) {
+        $(tabLinks[j]).on("click", this, openTab);
+      }
 
-  tabLinks[indexOfTargetTab].className = "tabs-navigation__item tabs-navigation__item--active";
-  tabContent[indexOfTargetTab].className = "tab-panel";
-}
+      for (var i = 0; i < tabContent.length; i++) {
+        $(tabContent[i]).toggleClass("hidden");
+      }
+    }
 
-function openTabByDefault() {
-  for (var i = 0; i < tabContent.length; i++) {
-    tabContent[i].className = "tab-panel hidden";
-  }
+    function displayFirstTabByDefault() {
+      $(tabLinks[0]).toggleClass("tabs-navigation__item--active");
+      $(tabContent[0]).toggleClass("hidden");
+    }
 
-  tabLinks[0].className = "tabs-navigation__item tabs-navigation__item--active";
-  tabContent[0].className = "tab-panel";
-}
+    addEventListeners();
+    displayFirstTabByDefault();
+  };
 
-openTabByDefault();
+  o.mobileNav = function () {
+    var mobileNavSwitcher = $(".mobile-nav");
+
+    function toggleMobileNav(event) {
+      var mobileMenu = event.currentTarget;
+      if ($(mobileMenu).hasClass('mobile-nav--expanded')) {
+        $(mobileMenu).animate({
+          width: "50"
+        }, 300, function () {
+          $(this).removeClass('mobile-nav--expanded');
+        });
+      } else {
+        $(mobileMenu).addClass('mobile-nav--expanded').animate({
+          width: "100%"
+        }, 300);
+      }
+    }
+
+    mobileNavSwitcher.on("click", this, toggleMobileNav);
+  };
+
+  return o;
+})($);
+
+$(document).ready(function () {
+  lifeBlog.showTabs();
+  lifeBlog.mobileNav();
+});
