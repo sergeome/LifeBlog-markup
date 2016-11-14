9c2e61e1e13dd12996d0283109fd4ed8d2010747
sergeome <sergeome@gmail.com>
2016-11-13 20:18:11 -0600

    Added comments to the code.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 lifeBlog = (function ($) {
   var o = {};
 
+  //Tabs Switcher
   o.showTabs = function () {
     var tabContent = $(".tab-panel");
     var tabLinks = $(".tabs-navigation__item");
@@ -10,7 +11,7 @@ lifeBlog = (function ($) {
       var indexOfTargetTab;
       var clickedTab = event.currentTarget;
 
-
+      //Get Index of clicked tab
       for (var z = 0; z < tabLinks.length; z++) {
         if (tabLinks[z] === clickedTab) {
           indexOfTargetTab = z;
@@ -18,36 +19,42 @@ lifeBlog = (function ($) {
         }
       }
 
+      //Hide all tabs
       for (var i = 0; i < tabContent.length; i++) {
         tabContent[i].className = "tab-panel hidden";
       }
 
+      //Assign Default Class to All Tab Links
       for (var j = 0; j < tabLinks.length; j++) {
         tabLinks[j].className = "tabs-navigation__item";
       }
 
+      //Enable clicked tab and highlighting tab link using its index
       $(tabLinks[indexOfTargetTab]).toggleClass("tabs-navigation__item--active");
       $(tabContent[indexOfTargetTab]).toggleClass("hidden");
     }
 
+    //Add "click" event listener to each tab
     function addEventListeners() {
-
       for (var j = 0; j < tabLinks.length; j++) {
         $(tabLinks[j]).on("click", this, openTab);
       }
+    }
 
+    //Applying default configuration
+    function openDefaultTab() {
+      //hide all tabs
       for (var i = 0; i < tabContent.length; i++) {
         $(tabContent[i]).toggleClass("hidden");
       }
-    }
 
-    function displayFirstTabByDefault() {
+      //Show first tab
       $(tabLinks[0]).toggleClass("tabs-navigation__item--active");
       $(tabContent[0]).toggleClass("hidden");
     }
 
     addEventListeners();
-    displayFirstTabByDefault();
+    openDefaultTab();
   };
 
   o.mobileNav = function () {
@@ -56,13 +63,18 @@ lifeBlog = (function ($) {
 
     function toggleMobileNav(event) {
       var mobileMenu = event.currentTarget;
+
+      //If Mobile menu is opened (i.e. has mobile-nav--expanded class)
       if ($(mobileMenu).hasClass('mobile-nav--expanded')) {
+        //Animate to slow hide it smoothly
         $(mobileMenu).animate({
           width: "50px",
           marginRight: "30px"
         }, 300, function () {
+          //When animation is finished remove class
           $(this).removeClass('mobile-nav--expanded');
         });
+        //And also hide navigation list smoothly
         $(topNavList).animate({
           width: "0",
           right: "30px"
@@ -70,13 +82,18 @@ lifeBlog = (function ($) {
           $(topNavList).css("display", "none");
         });
       } else {
+        //If Mobile menu isn't opened (i.e. doesn't have mobile-nav--expanded class)
+        //Smoothly add it
         $(mobileMenu).addClass('mobile-nav--expanded').animate({
           width: "100%",
           marginRight: "0"
         }, 300);
+        //Get Height of the body in order to expand navigation list full screen
         var navListHeight = $("body").height() - 321;
+        //Set CSS for navigation list
         $(topNavList).css("display", "block");
         $(topNavList).css("height", navListHeight);
+        //Show navigation list smoothly
         $(topNavList).animate({
           width: "80%",
           right: "0"
@@ -84,6 +101,7 @@ lifeBlog = (function ($) {
       }
     }
 
+    //Add "click" Event Listener to the Mobile Menu button
     mobileNavSwitcher.on("click", this, toggleMobileNav);
   };
 
